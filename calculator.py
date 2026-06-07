33ebb3959fab483729d7f98ad2f38689e973b62e
shabareesh505 <16shabareesh@gmail.com>
2026-06-07 19:38:37 +0530

    initial calculator setup

---
+++ calculator.py
@@ -0,0 +1,7 @@
+# this is user to calculate sum of two numbers
+def add(a,b):
+    return a+b
+inp1 = int(input("Enter a number"))
+inp2 = int(input("Enter another number"))
+print("the sum of two numbers are:",add(inp1,inp2))
+
