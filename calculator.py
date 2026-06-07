856a83575bc5abf422728a6c7e8d0ab93ac9c643
shabareesh505 <16shabareesh@gmail.com>
2026-06-07 21:45:07 +0530

    added multiplication feature

--- calculator.py
+++ calculator.py
@@ -1,7 +1,13 @@
-# this is user to calculate sum of two numbers
+# this is used to calculate sum of two numbers
 def add(a,b):
     return a+b
 inp1 = int(input("Enter a number"))
 inp2 = int(input("Enter another number"))
 print("the sum of two numbers are:",add(inp1,inp2))
 
+#this is used to calculate multiplication for two numbers
+def mul(a,b):
+    return a*b
+inp1 = int(input("Enter a number"))
+inp2 = int(input("Enter another number for multiplication"))
+print("the multiplication of two numbers are:",mul(inp1,inp2))
