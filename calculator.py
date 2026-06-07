a356954447030e833930531353f7544e08424cb9
shabareesh505 <16shabareesh@gmail.com>
2026-06-07 21:38:15 +0530

    Added substraction feature

--- calculator.py
+++ calculator.py
@@ -5,9 +5,18 @@ inp1 = int(input("Enter a number"))
 inp2 = int(input("Enter another number"))
 print("the sum of two numbers are:",add(inp1,inp2))
 
+
 #this is used to calculate multiplication for two numbers
 def mul(a,b):
     return a*b
 inp1 = int(input("Enter a number"))
 inp2 = int(input("Enter another number for multiplication"))
 print("the multiplication of two numbers are:",mul(inp1,inp2))
+
+#this is used to calculate substraction of two numbers
+def sub(a,b):
+    return a-b
+inp1 = int(input("Enter a number"))
+inp2 = int(input("Enter another number to substraction"))
+print("the substraction of two numbers are:",sub(inp1,inp2))
+
